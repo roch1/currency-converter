3386c362915746d3f40e44e4b3a60daa9e460fbf
roch1 <45519595+roch1@users.noreply.github.com>
2019-07-15 01:53:36 +0100

    Add Spring Boot dependencies
    - Added Spring Boot dependencies to POM.
    - Added "hello world" Spring Boot controller to verify setup is correct.

---
+++ src/main/java/web/app/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package web.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello, Spring Boot World!";
+    }
+
+}
