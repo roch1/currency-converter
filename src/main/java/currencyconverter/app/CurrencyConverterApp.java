55e528091355acb445486a7f44966570cc70bd94
roch1 <45519595+roch1@users.noreply.github.com>
2020-02-08 18:37:13 +0000

    Update Spring Boot version
    - Updated Spring Boot to latest version.
    - Renamed some classes and corresponding tests.

--- src/main/java/currencyconverter/app/CurrencyConverterApp.java
+++ src/main/java/currencyconverter/app/CurrencyConverterApp.java
@@ -4,7 +4,6 @@ import currencyconverter.data.DataStore;
 import currencyconverter.data.feeds.DataFeed;
 import currencyconverter.data.feeds.DataFeedManager;
 import currencyconverter.data.feeds.EuropeanCentralBank;
-import currencyconverter.service.Converter;
 import currencyconverter.service.RateScheduler;
 
 import java.math.BigDecimal;
@@ -14,22 +13,21 @@ import java.util.List;
 public class CurrencyConverter {
 
     public static void main(String[] args) {
-        DataStore datastore = new DataStore();
-
         List<DataFeed> dataFeeds = Arrays.asList(new EuropeanCentralBank());
         DataFeedManager dataFeedManager = new DataFeedManager(dataFeeds);
 
+        DataStore datastore = new DataStore();
         RateScheduler runner = new RateScheduler(datastore, dataFeedManager);
         runner.startScheduling();
 
-        Converter converter = new Converter(datastore);
+        currencyconverter.service.CurrencyConverter currencyConverter = new currencyconverter.service.CurrencyConverter(datastore);
 
         // test cases
-        System.out.println(converter.convert("hjk", "GBP", BigDecimal.TEN));
-        System.out.println(converter.convert("USD", "GBP", BigDecimal.TEN));
-        System.out.println(converter.convert("USd", "GbP", BigDecimal.TEN));
-        System.out.println(converter.convert(null, null, null));
-        System.out.println(converter.convert("USD", "JPY", BigDecimal.ZERO));
+        System.out.println(currencyConverter.convert("hjk", "GBP", BigDecimal.TEN));
+        System.out.println(currencyConverter.convert("USD", "GBP", BigDecimal.TEN));
+        System.out.println(currencyConverter.convert("USd", "GbP", BigDecimal.TEN));
+        System.out.println(currencyConverter.convert(null, null, null));
+        System.out.println(currencyConverter.convert("USD", "JPY", BigDecimal.ZERO));
 
     }
 
